26ca2e81054f7368fab3d2910f9dd45a8823e511
Alex_Ber <45004572+alextestgit@users.noreply.github.com>
2022-12-04 21:03:16 +0200

    add ArrayProgrammingQuestions.py

--- ArrayProgrammingQuestions.py
+++ ArrayProgrammingQuestions.py
@@ -3,7 +3,7 @@ test = {1: [[0, 1, 2, 3, 5, 6, 8, 10], [4, 7]]}
 
 
 def missing_number(arr):
-    print([item for item in range(arr[0],arr[:-1]+1) if item not in arr])
+    print([a for a in range(arr[0],arr[-1]+1) if a not in arr])
 
 
 missing_number([2, 1, 0, 3, 5, 6, 8, 10])
