2e2c06c4f82cb3340a71be2f7bdec27321079b65
Alex_Ber <45004572+alextestgit@users.noreply.github.com>
2022-12-04 20:01:35 +0200

    add ArrayProgrammingQuestions.py

---
+++ ArrayProgrammingQuestions.py
@@ -0,0 +1,9 @@
+# How do you find the missing number in a given integer array of 1 to 100
+test = {1: [[0, 1, 2, 3, 5, 6, 8, 10], [4, 7]]}
+
+
+def missing_number(arr):
+    print([item for item in range(arr[0],arr[:-1]+1) if item not in arr])
+
+
+missing_number([2, 1, 0, 3, 5, 6, 8, 10])
